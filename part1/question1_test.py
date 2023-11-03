8c0533ea07eb1ce7efd3f5007498cae36e493e8e
andres briñez <brinezlopez08@gmail.com>
2023-11-03 11:36:58 -0500

    Se agregan comentarios al test

--- part1/question1_test.py
+++ part1/question1_test.py
@@ -1,5 +1,7 @@
+# Prueba unitaria para la función get_city_weather  en el módulo question1
 from question1 import get_city_weather
 
+# Retorna resultados de la prueba unitaria  para las ciudades 
 def test_get_city_weather():
 
   assert get_city_weather("Quito") == "22 degrees and sunny"
