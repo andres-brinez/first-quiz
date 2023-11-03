e76f6f89137cfcb55f6b8ee8973e160893482e9b
andres briñez <brinezlopez08@gmail.com>
2023-11-03 11:44:18 -0500

    Se agregan comentarios

--- part1/question2_test.py
+++ part1/question2_test.py
@@ -1,6 +1,8 @@
 from question2 import run_swapper 
 
+# se prueba la funcion run_swapper con diferentes casos
 def test_run_swapper():
+  # Toma una lista de tuplas y devuelve una lista de tuplas con los valores intercambiados
   assert run_swapper(
     [ ("a", "b"), ("c", "d"), ("e", "f") ]
   ) == [ ("b", "a"), ("d", "c"), ("f", "e")]
