05cf0ad9ca467ee37a296f49d8e29fca4981090b
andres briñez <brinezlopez08@gmail.com>
2023-11-03 11:38:04 -0500

    Se corrige el error

--- part1/question1.py
+++ part1/question1.py
@@ -13,23 +13,37 @@
 # need to be fixed. The test suite in `question1_test.py` will verify the output.
 # Read the test suite to know the values that these functions should return.
 
+# Función que retorna la temperatura de una ciudad
+# Se completa el código para las ciudades que faltan
 def get_city_temperature(city):
+
    if city == "Quito":
       return 22
+   if city == "New York":
+      return 14
    if city == "Sao Paulo":
       return 17
    if city == "San Francisco":
       return 16
 
+
 def get_city_weather(city):
 
-  sky_condition = None
+   sky_condition = None
+   
+   if city == "Quito":
+      sky_condition = "sunny"
+   elif city == "Sao Paulo":
+      sky_condition = "cloudy"
+   elif city == "New York":
+      sky_condition = "rainy"
+   elif city == "San Francisco":
+      sky_condition = "sunny"
 
-  if city == "Sao Paulo":
-     sky_condition = "cloudy"
-  elif city == "New York":
-     sky_condition = "rainy"
+   temperature = get_city_temperature(city)
 
-  temperature = get_city_temperature(city)
+   return str(temperature) + " degrees and " + sky_condition
 
-  return str(temperature) + " degrees and " + sky_condition
+   # El error era que estaba retornando un NoneType, porque las ciudades del test no estaban en la función
+
+  
